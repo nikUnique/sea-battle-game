d99200a11782b58aeea954848f27fcdf26da728a
nik <nikutkov@gmail.com>
2024-02-20 17:15:16 +0600

    shootingLogic - comments written

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -1831,51 +1831,66 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "default", ()=>function(fleet, ships) {
         const shootingLogic = function(e) {
             e.preventDefault();
+            // Here are possible 2 options: first is that this variable will be a truthy value because it will successfully select desired element, but second options is that this value will be undefined. So, both of this values make it enough to select the right cell. For example if my opponent shot in a cell of enemySideMyFleet which is his side then that spot will be selected because it happened on his side, but if I shot a cell of mySideEnemyFleet then it will be undefined because closest method will not find enemySideMyFleet in mySideEnemyFleet, so, which means that this is the second option and a cell of mySideEnemyFleet will be chosen when this variable will be used
             const selectChosenCell = e.target.closest(".enemy-side--my-fleet")?.querySelector(`.${e.target.classList[0]}`);
             console.log(e.target, "target");
             const containsShip = !(0, _gameStartControl.playingCheck).playing || e.target.querySelector(".ship")?.classList.contains("ship");
+            // If I made a shot in dropzone containing ship instead of ship itself then this will not count and you can shoot again. This is done this way to make it simplier to select empty cell inside of dropzone which doesn't contain a ship
             if (containsShip) {
                 console.log("dropzone");
                 return;
             }
             const miss = "&bull;";
             const addMarkToFleet = function(fleet) {
+                // If the first condition is true this means that the shot missed and reached dropzone containing empty cell
                 if (e.target.classList[0] === "dropzone") return fleet.querySelector(`.${e.target.querySelector("div").classList[0]}`);
+                // If this is true then this means that the shot damaged a ship
                 if (e.target.classList[0] !== "dropzone") return fleet.querySelector(`.${e.target.classList[0]}`);
             };
             const addMissMark = function() {
                 if (e.target.textContent !== "") return;
+                // This and a piece of code below add a class and insert textContent in the fleet which was clicked
                 e.target.querySelector("div").classList.add("miss");
                 e.target.querySelector("div").insertAdjacentHTML("afterbegin", miss);
+                // These 2 if-statements basically do the same thing as above but duplicating this to the second sea part
                 if (selectChosenCell) {
-                    console.log(`.${e.target.classList[0]}`);
                     addMarkToFleet((0, _globalVars.mySideMyFleet)).classList.add("miss");
                     addMarkToFleet((0, _globalVars.mySideMyFleet)).insertAdjacentHTML("afterbegin", miss);
                 }
                 if (!selectChosenCell) {
-                    console.log(e.target.closest(".enemy-side--my-fleet"));
                     addMarkToFleet((0, _globalVars.enemySideEnemyFleet)).classList.add("miss");
                     addMarkToFleet((0, _globalVars.enemySideEnemyFleet)).insertAdjacentHTML("afterbegin", miss);
                 }
             };
+            // Calls the function when player misses ship
             !e.target.closest(".ship") && addMissMark();
-            if (e.target.closest(".ship")?.textContent !== "") return;
-            const injuredShipPos = ships.findIndex((ship)=>{
+            // If cell in which you shot is already marked then code execution will stop here because the code below marks damaged ships which is already done at this point if target is a marked ship part and if not then the cell was already marked as missed which means that textContent of the target is already not empty in these cases
+            if (e.target.textContent !== "") {
+                console.log(e.target);
+                console.log("You already shot that cell or you missed");
+                return;
+            }
+            const injuredShipPartPos = ships.findIndex((ship)=>{
+                // Find ship index in the ships arr which was damaged
                 return ship?.coords?.includes(e.target.classList[0]);
             });
             console.log(ships);
             e.target.classList.add("injure");
             const injure = "&cross;";
             // e.target.insertAdjacentHTML("afterbegin", injure);
+            // A nice neat trick when you don't need empty string but you also don't need it to be filled with something visible, so you just add empty space
             e.target.textContent = " ";
-            const destroyedShipCoords = ships[injuredShipPos].coords.map((_, i)=>{
-                return fleet.querySelector(`.${ships[injuredShipPos]?.coords[i]}`).nextElementSibling.classList.contains("injure");
+            // Checks whether ship is only damaged or destroyed completely
+            const destroyedShipCoords = ships[injuredShipPartPos].coords.map((_, i)=>{
+                return fleet.querySelector(`.${ships[injuredShipPartPos]?.coords[i]}`).nextElementSibling.classList.contains("injure");
             });
+            // Duplicate damage mark on the second sea part
             if (selectChosenCell) {
                 /* addMarkToFleet(mySideMyFleet).nextElementSibling.insertAdjacentHTML(
         "afterbegin",
         injure
       ), && */ addMarkToFleet((0, _globalVars.mySideMyFleet)).nextElementSibling.classList.add("injure");
+                // When a ship is damaged or destroyed then timer refreshes
                 (0, _helpers.startTimer)(fleet);
             }
             if (!selectChosenCell) {
@@ -1884,25 +1899,30 @@ parcelHelpers.export(exports, "default", ()=>function(fleet, ships) {
       ).nextElementSibling.insertAdjacentHTML("afterbegin", injure), && */ addMarkToFleet((0, _globalVars.enemySideEnemyFleet)).nextElementSibling.classList.add("injure");
                 (0, _helpers.startTimer)(fleet);
             }
+            // If ships is destroyed completely it's time to add border to it, but if not then execution stops here
             if (destroyedShipCoords.includes(false)) return;
             const addBorder = function(borderSide, coord, color = "#FA5252") {
                 const selectTd = function(fleetSide) {
                     fleetSide.querySelector(`.${coord}`).closest(".dropzone").style[borderSide] = `2px solid ${color} `;
                 };
                 selectTd(fleet);
+                // Select tds for adding border to ships of the second sea part
                 selectTd(fleet === (0, _globalVars.enemySideMyFleet) ? (0, _globalVars.mySideMyFleet) : (0, _globalVars.enemySideEnemyFleet));
             };
-            ships[injuredShipPos].coords.map((coord, i, arr)=>{
+            ships[injuredShipPartPos].coords.map((coord, i, arr)=>{
                 (0, _helpers.buildShipBorder)([
-                    ships[injuredShipPos],
+                    ships[injuredShipPartPos],
                     coord,
                     i,
                     arr,
                     addBorder
                 ]);
-                if (ships[injuredShipPos].coords.length !== 1) return;
+                // Code below if-statement will be executed only for 1-cell ships
+                if (ships[injuredShipPartPos].coords.length !== 1) return;
+                // Check whether 1-cell ship contains reward class or not
                 const rewardShip = fleet.querySelector(`.${coord}`).nextElementSibling.classList.contains("reward");
                 if (!rewardShip) return;
+                // Adds special class which is necessary for reward feature
                 fleet.classList.add("binoculars");
                 const labelBinocularsReward = fleet.closest(".sea-container").querySelector(".binoculars-reward-label");
                 const labelTimer = fleet.closest(".sea-container").querySelector(".timer");
@@ -1913,34 +1933,31 @@ parcelHelpers.export(exports, "default", ()=>function(fleet, ships) {
                         clearInterval(timer);
                         labelBinocularsReward.style.opacity = "0";
                         fleet.classList.remove("binoculars");
-                        console.log("Magic item removed");
+                        console.log("Magic video camera removed");
                     }
                     time--;
                 };
                 let time = (0, _config.TIME_LENGTHS).bonusTime;
                 tick();
+                // This will make timer
                 const timer = setInterval(tick, 1000);
             });
-            const destroyedShip = ships[injuredShipPos].coords.map((cell, i)=>{
-                return fleet.querySelector(`.${ships[injuredShipPos]?.coords[i]}`);
-            });
-            const filledAreaAroundShip = ships[injuredShipPos].unavailabeCells.filter((cell)=>{
+            const filledAreaAroundShip = ships[injuredShipPartPos].unavailabeCells.filter((cell)=>{
                 // Filtering out coords on which the ship inself is placed, because unavailableCells also included them
-                return !ships[injuredShipPos].coords.includes(cell);
+                return !ships[injuredShipPartPos].coords.includes(cell);
             }).filter((cell)=>{
                 return fleet.querySelector(`.${cell}`)?.textContent === "";
-            }).map((cell, i, arr)=>{
-                console.log(new Date());
+            }).map((cell, i)=>{
                 const cellAround = fleet.querySelector(`.${cell}`);
                 console.log(cellAround, "cellAround");
-                // There is also can be an imaginary 11th cell when it comes to side ships(because unavailableCells contains them, but only for conveniency reason), so there is a check whether that cell exists or not, because there is no 11th cell exists in the sea(Means that this could be misunderstood as if 11th cell exists but transparent)
+                // There is also can be an imaginary 11th cell when it comes to bottom ships(because unavailableCells contains them, but only for conveniency reason), so there is a check whether that cell exists or not, because there is no 11th cell exists in the sea(Means that this could be misunderstood as if 11th cell exists but transparent)
                 cellAround && (cellAround.style.fontSize = "3.2rem");
                 const surroundDestroyedShip = function(fleet, cellAround) {
                     cellAround?.textContent === "" && fleet.querySelector(`.${cell}`)?.insertAdjacentHTML("afterbegin", miss);
                     !cellAround?.classList.contains("miss") && cellAround?.classList.add("cell-around");
                     cellAround && (cellAround.style.fontSize = "3.2rem");
-                    cellAround.classList.contains("injure") && (cellAround.style.fontSize = "3.2rem");
                     cellAround.style.visibility = "hidden";
+                    // This is done for nice animation effect
                     setTimeout(function() {
                         cellAround.style.visibility = "visible";
                     }, i * (0, _config.APPEAR_TIME));
@@ -1954,8 +1971,7 @@ parcelHelpers.export(exports, "default", ()=>function(fleet, ships) {
                 // If the cell is empty then a new mark will be inserted, but if there is something inside then nothing will happen
                 surroundDestroyedShip(fleet, cellAround);
             });
-            /**************************/ /* CONTROLLING THE END OF THE GAME */ /**************************/ console.log("end");
-            (0, _showEndResultsDefault.default)(fleet);
+            /**************************/ /* CONTROLLING THE END OF THE GAME */ /**************************/ (0, _showEndResultsDefault.default)(fleet);
         };
         fleet.addEventListener("click", function(e) {
             shootingLogic(e);
--- src/js/shootingLogic.js
+++ src/js/shootingLogic.js
@@ -16,6 +16,7 @@ export default function (fleet, ships) {
   const shootingLogic = function (e) {
     e.preventDefault();
 
+    // Here are possible 2 options: first is that this variable will be a truthy value because it will successfully select desired element, but second options is that this value will be undefined. So, both of this values make it enough to select the right cell. For example if my opponent shot in a cell of enemySideMyFleet which is his side then that spot will be selected because it happened on his side, but if I shot a cell of mySideEnemyFleet then it will be undefined because closest method will not find enemySideMyFleet in mySideEnemyFleet, so, which means that this is the second option and a cell of mySideEnemyFleet will be chosen when this variable will be used
     const selectChosenCell = e.target
       .closest(".enemy-side--my-fleet")
       ?.querySelector(`.${e.target.classList[0]}`);
@@ -26,6 +27,7 @@ export default function (fleet, ships) {
       !playingCheck.playing ||
       e.target.querySelector(".ship")?.classList.contains("ship");
 
+    // If I made a shot in dropzone containing ship instead of ship itself then this will not count and you can shoot again. This is done this way to make it simplier to select empty cell inside of dropzone which doesn't contain a ship
     if (containsShip) {
       console.log("dropzone");
       return;
@@ -34,11 +36,13 @@ export default function (fleet, ships) {
     const miss = "&bull;";
 
     const addMarkToFleet = function (fleet) {
+      // If the first condition is true this means that the shot missed and reached dropzone containing empty cell
       if (e.target.classList[0] === "dropzone")
         return fleet.querySelector(
           `.${e.target.querySelector("div").classList[0]}`
         );
 
+      // If this is true then this means that the shot damaged a ship
       if (e.target.classList[0] !== "dropzone")
         return fleet.querySelector(`.${e.target.classList[0]}`);
     };
@@ -48,21 +52,19 @@ export default function (fleet, ships) {
         return;
       }
 
+      // This and a piece of code below add a class and insert textContent in the fleet which was clicked
       e.target.querySelector("div").classList.add("miss");
 
       e.target.querySelector("div").insertAdjacentHTML("afterbegin", miss);
 
+      // These 2 if-statements basically do the same thing as above but duplicating this to the second sea part
       if (selectChosenCell) {
-        console.log(`.${e.target.classList[0]}`);
-
         addMarkToFleet(mySideMyFleet).classList.add("miss");
 
         addMarkToFleet(mySideMyFleet).insertAdjacentHTML("afterbegin", miss);
       }
 
       if (!selectChosenCell) {
-        console.log(e.target.closest(".enemy-side--my-fleet"));
-
         addMarkToFleet(enemySideEnemyFleet).classList.add("miss");
 
         addMarkToFleet(enemySideEnemyFleet).insertAdjacentHTML(
@@ -72,13 +74,18 @@ export default function (fleet, ships) {
       }
     };
 
+    // Calls the function when player misses ship
     !e.target.closest(".ship") && addMissMark();
 
-    if (e.target.closest(".ship")?.textContent !== "") {
+    // If cell in which you shot is already marked then code execution will stop here because the code below marks damaged ships which is already done at this point if target is a marked ship part and if not then the cell was already marked as missed which means that textContent of the target is already not empty in these cases
+    if (e.target.textContent !== "") {
+      console.log(e.target);
+      console.log("You already shot that cell or you missed");
       return;
     }
 
-    const injuredShipPos = ships.findIndex((ship) => {
+    const injuredShipPartPos = ships.findIndex((ship) => {
+      // Find ship index in the ships arr which was damaged
       return ship?.coords?.includes(e.target.classList[0]);
     });
 
@@ -89,14 +96,17 @@ export default function (fleet, ships) {
 
     // e.target.insertAdjacentHTML("afterbegin", injure);
 
+    // A nice neat trick when you don't need empty string but you also don't need it to be filled with something visible, so you just add empty space
     e.target.textContent = " ";
 
-    const destroyedShipCoords = ships[injuredShipPos].coords.map((_, i) => {
+    // Checks whether ship is only damaged or destroyed completely
+    const destroyedShipCoords = ships[injuredShipPartPos].coords.map((_, i) => {
       return fleet
-        .querySelector(`.${ships[injuredShipPos]?.coords[i]}`)
+        .querySelector(`.${ships[injuredShipPartPos]?.coords[i]}`)
         .nextElementSibling.classList.contains("injure");
     });
 
+    // Duplicate damage mark on the second sea part
     if (selectChosenCell) {
       /* addMarkToFleet(mySideMyFleet).nextElementSibling.insertAdjacentHTML(
         "afterbegin",
@@ -104,6 +114,7 @@ export default function (fleet, ships) {
       ), && */
       addMarkToFleet(mySideMyFleet).nextElementSibling.classList.add("injure");
 
+      // When a ship is damaged or destroyed then timer refreshes
       startTimer(fleet);
     }
 
@@ -118,6 +129,7 @@ export default function (fleet, ships) {
       startTimer(fleet);
     }
 
+    // If ships is destroyed completely it's time to add border to it, but if not then execution stops here
     if (destroyedShipCoords.includes(false)) return;
 
     const addBorder = function (borderSide, coord, color = "#FA5252") {
@@ -129,18 +141,21 @@ export default function (fleet, ships) {
 
       selectTd(fleet);
 
+      // Select tds for adding border to ships of the second sea part
       selectTd(
         fleet === enemySideMyFleet ? mySideMyFleet : enemySideEnemyFleet
       );
     };
 
-    ships[injuredShipPos].coords.map((coord, i, arr) => {
-      buildShipBorder([ships[injuredShipPos], coord, i, arr, addBorder]);
+    ships[injuredShipPartPos].coords.map((coord, i, arr) => {
+      buildShipBorder([ships[injuredShipPartPos], coord, i, arr, addBorder]);
 
-      if (ships[injuredShipPos].coords.length !== 1) {
+      // Code below if-statement will be executed only for 1-cell ships
+      if (ships[injuredShipPartPos].coords.length !== 1) {
         return;
       }
 
+      // Check whether 1-cell ship contains reward class or not
       const rewardShip = fleet
         .querySelector(`.${coord}`)
         .nextElementSibling.classList.contains("reward");
@@ -148,7 +163,7 @@ export default function (fleet, ships) {
       if (!rewardShip) {
         return;
       }
-
+      // Adds special class which is necessary for reward feature
       fleet.classList.add("binoculars");
 
       const labelBinocularsReward = fleet
@@ -171,37 +186,33 @@ export default function (fleet, ships) {
 
           fleet.classList.remove("binoculars");
 
-          console.log("Magic item removed");
+          console.log("Magic video camera removed");
         }
 
         time--;
       };
+
       let time = TIME_LENGTHS.bonusTime;
       tick();
+
+      // This will make timer
       const timer = setInterval(tick, 1000);
     });
 
-    const destroyedShip = ships[injuredShipPos].coords.map((cell, i) => {
-      return fleet.querySelector(`.${ships[injuredShipPos]?.coords[i]}`);
-    });
-
-    const filledAreaAroundShip = ships[injuredShipPos].unavailabeCells
+    const filledAreaAroundShip = ships[injuredShipPartPos].unavailabeCells
       .filter((cell) => {
         // Filtering out coords on which the ship inself is placed, because unavailableCells also included them
-        return !ships[injuredShipPos].coords.includes(cell);
+        return !ships[injuredShipPartPos].coords.includes(cell);
       })
       .filter((cell) => {
         return fleet.querySelector(`.${cell}`)?.textContent === "";
       })
-
-      .map((cell, i, arr) => {
-        console.log(new Date());
-
+      .map((cell, i) => {
         const cellAround = fleet.querySelector(`.${cell}`);
 
         console.log(cellAround, "cellAround");
 
-        // There is also can be an imaginary 11th cell when it comes to side ships(because unavailableCells contains them, but only for conveniency reason), so there is a check whether that cell exists or not, because there is no 11th cell exists in the sea(Means that this could be misunderstood as if 11th cell exists but transparent)
+        // There is also can be an imaginary 11th cell when it comes to bottom ships(because unavailableCells contains them, but only for conveniency reason), so there is a check whether that cell exists or not, because there is no 11th cell exists in the sea(Means that this could be misunderstood as if 11th cell exists but transparent)
         cellAround && (cellAround.style.fontSize = "3.2rem");
 
         const surroundDestroyedShip = function (fleet, cellAround) {
@@ -215,11 +226,9 @@ export default function (fleet, ships) {
 
           cellAround && (cellAround.style.fontSize = "3.2rem");
 
-          cellAround.classList.contains("injure") &&
-            (cellAround.style.fontSize = "3.2rem");
-
           cellAround.style.visibility = "hidden";
 
+          // This is done for nice animation effect
           setTimeout(function () {
             cellAround.style.visibility = "visible";
           }, i * APPEAR_TIME);
@@ -244,7 +253,6 @@ export default function (fleet, ships) {
     /**************************/
     /* CONTROLLING THE END OF THE GAME */
     /**************************/
-    console.log("end");
     showEndResults(fleet);
   };
 
